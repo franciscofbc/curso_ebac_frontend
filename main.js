ff9a03d2c7405d88bf04de862146d5149aea1c09
Francisco Berti da Cruz <fbc991@live.com>
2023-01-17 15:48:17 -0300

    concluido

--- main.js
+++ main.js
@@ -1,29 +1,66 @@
-const form = document.getElementById("form")
-const valorA = document.getElementById("valor-a")
-const valorB = document.getElementById("valor-b")
-const mensagemSucesso = "Parabéns! Seu formulário foi submetido com sucesso"
-const mensagemInsucesso = "Formulário inválido. O Valor B deve ser maior que o Valor A"
-const containerMensagemSucesso = document.querySelector(".sucess-message")
-const containerMensagemInsucesso = document.querySelector(".error-message")
+const form = document.getElementById("form-atividade")
+const imgAprovado = '<img src="./images/aprovado.png" alt="Emoji aprovado">'
+const imgReprovado = '<img src="./images/reprovado.png" alt="Emoji reprovado>"'
+const atividades = []
+const notas = []
+const spanAprovado = '<span class="resultado aprovado">Aprovado</span>'
+const spanReprovado = '<span class="resultado reprovado">Reprovado</span>'
+const notaMinima = parseFloat(prompt("Digite a nota mínima:"))
+
+let linhas = ""
 
 form.addEventListener("submit", function (e) {
-    e.preventDefault();
+    /*Remoção quando submetido atualiza a tela*/
+    e.preventDefault()
 
-    if (parseInt(valorA.value) >= parseInt(valorB.value)) {
-        containerMensagemInsucesso.innerHTML = mensagemInsucesso
-        containerMensagemInsucesso.style.display = "block"
-        containerMensagemSucesso.style.display = "none"
-        limparForm(valorA, valorB)
-    } else {
-        containerMensagemSucesso.innerHTML = mensagemSucesso
-        containerMensagemSucesso.style.display = "block"
-        containerMensagemInsucesso.style.display = "none"
-        limparForm(valorA, valorB)
-    }
+    addLinha()
+    atualizaTabela()
+    atualizaMediaFinal()
+
 })
 
-function limparForm(a, b) {
-    a.value = ""
-    b.value = ""
+function addLinha() {
+    const inputNomeAtividade = document.getElementById("nome-atividade")
+    const inputNotaAtividade = document.getElementById("nota-atividade")
 
+    if (atividades.includes(inputNomeAtividade.value)) {
+        alert("ja inserida")
+    } else {
+        atividades.push(inputNomeAtividade.value)
+        notas.push(parseFloat(inputNotaAtividade.value))
+
+        let linha = "<tr>"
+        linha += `<td>${inputNomeAtividade.value}</td>`
+        linha += `<td>${inputNotaAtividade.value}</td>`
+        /*Operador ternário*/
+        linha += `<td>${inputNotaAtividade.value >= notaMinima ? imgAprovado : imgReprovado}</td>`
+        linha += "</tr>"
+
+        linhas += linha
+    }
+
+    inputNomeAtividade.value = ""
+    inputNotaAtividade.value = ""
+    /*alert(`Atividade: ${inputNomeAtividade.value}. Nota: ${inputNotaAtividade.value}`)*/
+}
+
+function atualizaTabela() {
+    const corpoTabela = document.querySelector("tbody")
+    corpoTabela.innerHTML = linhas
+}
+
+function atualizaMediaFinal() {
+    const mediaFinal = calculaMediaFinal()
+    document.getElementById("media-final-valor").innerHTML = mediaFinal
+    document.getElementById("media-final-resultado").innerHTML = mediaFinal >= notaMinima ? spanAprovado : spanReprovado
+}
+
+function calculaMediaFinal() {
+    let somaDasNotas = 0
+
+    for (let i = 0; i < notas.length; i++) {
+        somaDasNotas += notas[i]
+    }
+
+    return somaDasNotas / notas.length
 }
