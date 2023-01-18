dec0531027cec7b3cd77c9819289ece544dd67b9
Francisco Berti da Cruz <fbc991@live.com>
2023-01-17 22:28:20 -0300

    aula 1 ok

---
+++ main.js
@@ -0,0 +1,6 @@
+/*Tudo o que precisarmos do jQuery devemos colocar dentro deste campo*/
+$(document).ready(function () {
+    console.log(document.querySelector("header button"))
+    /*Forma que selecionamos elementos HTML atraves do jQuery. Podemos colocar qualquer tipo de seletor*/
+    console.log($("header button"))
+})
