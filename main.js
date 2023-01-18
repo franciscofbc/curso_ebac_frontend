572d009f4a74547f8d66c4e3847935355b6a12fe
Francisco Berti da Cruz <fbc991@live.com>
2023-01-18 16:25:57 -0300

    falta marcar lista como selecionada

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
-const form = document.getElementById("form")
-const valorA = document.getElementById("valor-a")
-const valorB = document.getElementById("valor-b")
-const mensagemSucesso = "Parabéns! Seu formulário foi submetido com sucesso"
-const mensagemInsucesso = "Formulário inválido. O Valor B deve ser maior que o Valor A"
-const containerMensagemSucesso = document.querySelector(".sucess-message")
-const containerMensagemInsucesso = document.querySelector(".error-message")
+$(document).ready(function () {
 
-form.addEventListener("submit", function (e) {
-    e.preventDefault();
+    $("form").on("submit", function (e) {
+        e.preventDefault()
 
-    if (parseInt(valorA.value) >= parseInt(valorB.value)) {
-        containerMensagemInsucesso.innerHTML = mensagemInsucesso
-        containerMensagemInsucesso.style.display = "block"
-        containerMensagemSucesso.style.display = "none"
-        limparForm(valorA, valorB)
-    } else {
-        containerMensagemSucesso.innerHTML = mensagemSucesso
-        containerMensagemSucesso.style.display = "block"
-        containerMensagemInsucesso.style.display = "none"
-        limparForm(valorA, valorB)
-    }
+        const nomeTarefa = $("#nome-tarefa").val()
+
+        const novoItem = $(`<li>${nomeTarefa}</li>`)
+        $(novoItem).appendTo("ul")
+
+        $("#nome-tarefa").val("")
+    })
+
 })
-
-function limparForm(a, b) {
-    a.value = ""
-    b.value = ""
-
-}
