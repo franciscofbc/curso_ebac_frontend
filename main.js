826513f89442164a262accb40591c75822fd2ba6
Francisco Berti da Cruz <fbc991@live.com>
2023-01-20 10:17:27 -0300

    concluido

--- main.js
+++ main.js
@@ -1,18 +1,5 @@
 $(document).ready(function () {
-    /*$("form").on("submit", function (e) {
-        e.preventDefault()
 
-        const inputNomeTarefa = $("#nome-tarefa").val()
-        const novaTarefa = $(`<li>${inputNomeTarefa}</li>`)
-
-        $(novaTarefa).appendTo("ul")
-
-        $("#nome-tarefa").val("")
-
-        $("li").click(function () {
-            $(this).addClass("riscado")
-        })
-    })*/
     $(".button-cadastrar").on("click", function (e) {
 
         e.preventDefault()
