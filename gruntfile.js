70a2509adb33cf274b72605c9f3880bbbf02618e
Francisco Berti da Cruz <fbc991@live.com>
2023-01-30 15:40:32 -0300

    ok depois do ignore

--- gruntfile.js
+++ gruntfile.js
@@ -33,6 +33,10 @@ module.exports = function (grunt) {
                         {
                             match: "ENDERECO_DO_CSS",
                             replacement: "./styles/main.css"
+                        },
+                        {
+                            match: "ENDERECO_DO_JS",
+                            replacement: "../src/scripts/main.js"
                         }
                     ]
                 },
@@ -51,6 +55,10 @@ module.exports = function (grunt) {
                         {
                             match: "ENDERECO_DO_CSS",
                             replacement: "./styles/main.min.css"
+                        },
+                        {
+                            match: "ENDERECO_DO_JS",
+                            replacement: "./scripts/main.min.js"
                         }
                     ]
                 },
@@ -77,7 +85,14 @@ module.exports = function (grunt) {
         },
         clean: [
             "prebuild"
-        ]
+        ],
+        uglify: {
+            target: {
+                files: {
+                    "dist/scripts/main.min.js": "src/scripts/main.js"
+                }
+            }
+        }
 
 
         /* ,
@@ -111,9 +126,10 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks("grunt-replace")
     grunt.loadNpmTasks("grunt-contrib-htmlmin")
     grunt.loadNpmTasks("grunt-contrib-clean")
+    grunt.loadNpmTasks("grunt-contrib-uglify")
 
     /* grunt.registerTask("default", ["concurrent"]) */
     /* grunt.registerTask("default", ["less:development"]) */
     grunt.registerTask("default", ["watch"])
-    grunt.registerTask("build", ["less:production", "htmlmin:dist", "replace:dist", "clean"])
+    grunt.registerTask("build", ["less:production", "htmlmin:dist", "replace:dist", "clean", "uglify"])
 }
